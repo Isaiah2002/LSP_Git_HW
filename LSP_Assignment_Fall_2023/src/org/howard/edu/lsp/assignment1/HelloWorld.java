0d479336ca21569d6c433a4fae1d4a9a418ddea1
wemote1592 <ibrundidge03@gmail.com>
2023-08-24 19:58:16 -0400

    Initial commit.

---
+++ LSP_Assignment_Fall_2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld 
+{
+	/**
+	 * Isaiah Brundidge
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
